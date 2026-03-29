af2f14ccf5dcdbdc9d2639ba17a12aa4a13880b8
SyedHannanMehdi <47053176+SyedHannanMehdi@users.noreply.github.com>
2026-03-30 01:36:20 +0530

    feat: add edit on GitHub link to every doc page (#65)
    Configure the editUrl in docusaurus.config.js so that every documentation
    page shows an "Edit this page" link pointing to the corresponding file in
    the tscircuit/docs-old GitHub repository.

---
+++ docusaurus.config.js
@@ -0,0 +1,124 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github")
+const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "tscircuit docs",
+  tagline: "Build electronics with TypeScript",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://docs.tscircuit.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "tscircuit", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you can set
+  // it to `zh-Hans`.
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // This enables the "Edit this page" link on every doc page
+          editUrl:
+            "https://github.com/tscircuit/docs-old/edit/main/",
+          routeBasePath: "/",
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/tscircuit-social-card.png",
+      navbar: {
+        title: "tscircuit",
+        logo: {
+          alt: "tscircuit Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            href: "https://github.com/tscircuit/tscircuit",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Getting Started",
+                to: "/",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.gg/tscircuit",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/tscircuit",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/tscircuit/tscircuit",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} tscircuit. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+}
+
+module.exports = config
